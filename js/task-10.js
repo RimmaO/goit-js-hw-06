fcd95d8dc7005041f577423784cd0df7f8755025
RimmaO <rimma_o@ukr.net>
2023-04-22 14:07:51 +0300

    update

--- js/task-10.js
+++ js/task-10.js
@@ -11,25 +11,18 @@ const refs = {
   boxes: document.getElementById("boxes"),
 };
 
-// const toggleValue = (event) => {
-//   let inputValue = 0;
-//   inputValue = event.currentTarget.value;
-// };
-
-// refs.input.addEventListener("input", toggleValue);
-
-const createBoxes = (ammount) => {
+const createBoxes = () => {
   let size = 30;
   const boxesArray = [];
-  for (let index = 0; index < ammount; index += 1) {
+  for (let index = 0; index < refs.input.value; index += 1) {
     size += 10 * index;
-    const div = `<div class="boxes-item" style="background-color: ${getRandomHexColor()}; width: ${size}; height:${size}"></div>`;
+    const div = `<div class="boxes-item" style="background-color: ${getRandomHexColor()}; width: ${size}px; height:${size}px"></div>`;
     boxesArray.push(div);
   }
   refs.boxes.insertAdjacentHTML("beforeend", boxesArray.join(""));
 };
 
-refs.create.addEventListener("click", createBoxes(+refs.input.value));
+refs.create.addEventListener("click", createBoxes);
 
 const destroyBoxes = () => {
   refs.input.value = "";
